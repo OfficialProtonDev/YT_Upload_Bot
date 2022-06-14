74b95d938791ea7447650b137c3eb800d01e9a3c
Elijah Harmon <elijahharmon@gmail.com>
2022-06-14 01:08:10 -0400

    pep8 formating and cleaned up login method to use attributes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from youtubeBot import YoutubeBot
 
+
 def main():
     email = "REPLACE_ME"
     password = "REPLACE_ME"
-    vid_path = r"REPLACE_ME"
+    vid_path = "REPLACE_ME"
     vid_title = "REPLACE_ME"
     vid_desc = "REPLACE_ME"
     loops = 1
--- youtubeBot.py
+++ youtubeBot.py
@@ -11,6 +11,7 @@ import time
 from dateutil.tz import *
 import time
 
+
 class YoutubeBot:
     def __init__(self, email, password, vid_path, vid_title, vid_desc, loops):
 
@@ -40,49 +41,53 @@ class YoutubeBot:
 
     def findByID(self, ID):
         while True:
-                try:
-                    found = self.driver.find_element(by=By.ID, value=ID)
-                    #print("found: " + ID)
-                    break
-                except exc.NoSuchElementException or exc.ElementClickInterceptedException:
-                    #print("finding: " + ID)
-                    time.sleep(1)
+            try:
+                found = self.driver.find_element(by=By.ID, value=ID)
+                #print("found: " + ID)
+                break
+            except exc.NoSuchElementException or exc.ElementClickInterceptedException:
+                #print("finding: " + ID)
+                time.sleep(1)
         return found
 
     def findByXPath(self, xpath):
         while True:
-                try:
-                    found = self.driver.find_element(by=By.XPATH, value=xpath)
-                    #print("found: " + xpath)
-                    break
-                except exc.NoSuchElementException or exc.ElementClickInterceptedException:
-                    #print("finding: " + xpath)
-                    time.sleep(1)
+            try:
+                found = self.driver.find_element(by=By.XPATH, value=xpath)
+                #print("found: " + xpath)
+                break
+            except exc.NoSuchElementException or exc.ElementClickInterceptedException:
+                #print("finding: " + xpath)
+                time.sleep(1)
         return found
 
     def findByName(self, name):
         while True:
-                try:
-                    found = self.driver.find_element(by=By.NAME, value=name)
-                    #print("found: " + name)
-                    break
-                except exc.NoSuchElementException or exc.ElementClickInterceptedException:
-                    #print("finding: " + name)
-                    time.sleep(1)
+            try:
+                found = self.driver.find_element(by=By.NAME, value=name)
+                #print("found: " + name)
+                break
+            except exc.NoSuchElementException or exc.ElementClickInterceptedException:
+                #print("finding: " + name)
+                time.sleep(1)
         return found
 
-    def googleLogin(self, mail_address, password):
+    def googleLogin(self):
         self.driver.get("https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fstudio.youtube.com%252F&hl=en&passive=false&service=youtube&uilel=0&flowName=GlifWebSignIn&flowEntry=AddSession")
 
-        emailid=self.driver.find_element(by=By.XPATH, value="//input[@name='identifier']")
-        emailid.send_keys(mail_address)
-        self.driver.find_element(by=By.XPATH, value="//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qfvgSe qIypjc TrZEUc lw1w4b']").click()
+        emailid = self.driver.find_element(
+            by=By.XPATH, value="//input[@name='identifier']")
+        emailid.send_keys(self.email)
+        self.driver.find_element(
+            by=By.XPATH, value="//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qfvgSe qIypjc TrZEUc lw1w4b']").click()
 
         time.sleep(3)
 
-        passw=self.driver.find_element(by=By.XPATH, value="//input[@name='password']")
-        passw.send_keys(password)
-        self.driver.find_element(by=By.XPATH, value="//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qfvgSe qIypjc TrZEUc lw1w4b']").click()
+        passw = self.driver.find_element(
+            by=By.XPATH, value="//input[@name='password']")
+        passw.send_keys(self.password)
+        self.driver.find_element(
+            by=By.XPATH, value="//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc qfvgSe qIypjc TrZEUc lw1w4b']").click()
 
         time.sleep(3)
 
@@ -93,7 +98,7 @@ class YoutubeBot:
                 #print("found it")
                 break
             except exc.NoSuchElementException:
-                #print("not done click again")  
+                #print("not done click again")
                 self.tryClick(element)
                 time.sleep(1)
         return found
@@ -106,7 +111,7 @@ class YoutubeBot:
             pass
 
     def upload_videos(self):
-        self.googleLogin(self.email, self.password)
+        self.googleLogin()
 
         for _ in range(int(self.loops)):
             # takes you to the upload page
@@ -117,49 +122,53 @@ class YoutubeBot:
 
             # click upload button
             uploadButton2 = self.findByID("text-item-0")
-            uploadButton2.click()    
+            uploadButton2.click()
 
             uploadFile = self.findByXPath("//input[@type='file']")
             uploadFile.send_keys(self.vid_path)
 
             # input title
-            titleInput = self.findByXPath("/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div")
+            titleInput = self.findByXPath(
+                "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[1]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div")
             titleInput.clear()
             titleInput.send_keys(self.vid_title)
 
             # input description
-            descInput = self.findByXPath("/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div")
+            descInput = self.findByXPath(
+                "/html/body/ytcp-uploads-dialog/tp-yt-paper-dialog/div/ytcp-animatable[1]/ytcp-ve/ytcp-video-metadata-editor/div/ytcp-video-metadata-editor-basics/div[2]/ytcp-social-suggestions-textbox/ytcp-form-input-container/div[1]/div[2]/div/ytcp-social-suggestion-input/div")
             descInput.clear()
             descInput.send_keys(self.vid_desc)
 
             # input not for kids
             notForKidsToggle = self.findByName("VIDEO_MADE_FOR_KIDS_NOT_MFK")
-            notForKidsToggle.click()           
+            notForKidsToggle.click()
 
             # click next button
             nextButton = self.findByID("next-button")
-            nextButton.click()      
+            nextButton.click()
 
             # click next button
             nextButton = self.findByID("next-button")
-            nextButton.click()        
+            nextButton.click()
 
             # click next button
             nextButton = self.findByID("next-button")
-            nextButton.click()  
+            nextButton.click()
 
-            # input public  
+            # input public
             privacyToggle = self.findByName("PUBLIC")
-            privacyToggle.click()          
-            
+            privacyToggle.click()
+
             # find publish button
             publishButton = self.findByID("done-button")
 
             # check if processing
-            self.findByXPath("//div[contains(text(), 'Processing will begin shortly')]")
+            self.findByXPath(
+                "//div[contains(text(), 'Processing will begin shortly')]")
 
             # click publish until the close button appears
-            closeButton = self.attemptClickUntilXPATHFound(publishButton, "/html/body/ytcp-uploads-still-processing-dialog/ytcp-dialog/tp-yt-paper-dialog/div[3]/ytcp-button")             
+            closeButton = self.attemptClickUntilXPATHFound(
+                publishButton, "/html/body/ytcp-uploads-still-processing-dialog/ytcp-dialog/tp-yt-paper-dialog/div[3]/ytcp-button")
 
             # click the close button
             closeButton.click()
